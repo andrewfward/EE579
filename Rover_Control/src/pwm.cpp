d63af4e07794b3fd473802884f0013802310bf35
Douglas02 <douglasfraser15@icloud.com>
2025-03-23 19:04:48 +0000

    updated motorStartupSequence to the new ESC (500 Hz , 1.5ms middle point ,75 % duty cycle)

--- Rover_Control/src/pwm.cpp
+++ Rover_Control/src/pwm.cpp
@@ -6,7 +6,7 @@ int motorStartupSequence() {
   unsigned long startMillis;
 
   // PWM settings
-  static int PWM_FREQ = 50;
+  static int PWM_FREQ = 500;
   static int PWM_RESOLUTION = 8;
 
   // Configure PWM
@@ -14,13 +14,13 @@ int motorStartupSequence() {
   ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
 
   // Set initial duty cycle to 7.5%
-  ledcWrite(PWM_CHANNEL, 7.5 * 255 / 100);
+  ledcWrite(PWM_CHANNEL, 75 * 255 / 100);
 
   // Run startup sequence for a period defined in the header file
   startMillis = millis();
   while (millis() - startMillis < STARTUP_SEQUENCE_LENGTH) {
   // while(1){
-    ledcWrite(PWM_CHANNEL, 7.5 * 255 / 100);
+    ledcWrite(PWM_CHANNEL, 75 * 255 / 100);
     delay(20);
   }
 
